064de0caf366678d6b9264cb3cc7d09bce42bfd2
Tayfun Akdeniz <45831526+bayek35@users.noreply.github.com>
2018-12-12 23:34:41 +0300

    def kodunu tryolarak değiştirdim :100:
    Co-Authored-By: orhan <orhan55555@users.noreply.github.com>

--- hesapmakinesi/hesapmakinesi/Form1.cs
+++ hesapmakinesi/hesapmakinesi/Form1.cs
@@ -26,7 +26,7 @@ namespace hesapmakinesi
 
         private void button1_Click(object sender, EventArgs e)
         {
-            def
+            try
             {
                 double a, b, top;
           
